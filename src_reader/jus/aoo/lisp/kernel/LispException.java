2472a9cad5a2bf44e0ac5052620b6fab151275c9
HugoGDA <gdahugo@gmail.com>
2017-03-29 16:17:20 +0200

    reader

---
+++ src_reader/jus/aoo/lisp/kernel/LispException.java
@@ -0,0 +1,29 @@
+package jus.aoo.lisp.kernel;
+
+public class LispException extends RuntimeException {
+
+	public LispException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public LispException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LispException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LispException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LispException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
