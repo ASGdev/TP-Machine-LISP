43db5c45433a7aa92e65f03e26ae9e08c8347c05
Aurélien Surier Garofalo <Aurlien Surier Garofalo>
2017-04-05 14:29:20 +0200

    Adding Context methods

--- src/jus/aoo/lisp/kernel/Context.java
+++ src/jus/aoo/lisp/kernel/Context.java
@@ -5,17 +5,17 @@ import java.util.TreeMap;
 
 public class Context {
 	public static TreeMap<Symbole, Sexpr> contexte = new TreeMap<Symbole, Sexpr>();
-	public static Sexpr get(Symbole s) {
-		return null;
+	public static Sexpr get(Symbole s) {	
+		return contexte.get(s);
 	}
 	public static void bind(Sexpr f, Sexpr e) {
 		
 	}
 	public static void unbind() {
-		
+		contexte.clear();		
 	}
 	public static void define (Symbole s, Sexpr exp) {
-		
+		contexte.put(s, exp);
 	}
 
 }
