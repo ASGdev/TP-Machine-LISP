4a38e046b2e14137b92657baf0e1d08c447c9abc
aurelien <root@aurelinux>
2017-04-12 14:41:22 +0200

    Added methodes

--- src/jus/aoo/lisp/kernel/_Reducer.java
+++ src/jus/aoo/lisp/kernel/_Reducer.java
@@ -2,4 +2,11 @@ package jus.aoo.lisp.kernel;
 
 public interface _Reducer {
 
+	public Sexpr reduction(Sexpr fct, Sexpr argList){
+		
+	}
+	
+	public sexpr apply(Sexpr fct, Sexpr argList){
+		
+	}
 }
